e7cc036fff1f9eaef9c826b435c5ebd3c92f1282
axelwass <axelwass@gmail.com>
2018-05-23 14:28:18 -0500

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        GitHub autoupdate! 5
+        GitHub autoupdate! 6
       </div>
       <div class="col-lg-6">
       </div>
